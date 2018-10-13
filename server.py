1ec576991cd248f53141db20398c5fc7cf7d71d1
Selim Enes Kılıçaslan <33657157+SelimEnes@users.noreply.github.com>
2018-10-13 16:49:42 +0300

    Login - Balance
    User details and balance

--- server.py
+++ server.py
@@ -9,6 +9,14 @@ app.secret_key = "secretkey123"
 dsn = """user='kbktqbcfmdxpbw' password='76006678dc4edef0501db56d75112cacde489dfb1be1648833f8ea853a1e32f4'
          host='ec2-54-247-101-191.eu-west-1.compute.amazonaws.com' port=5432 dbname='d1lo8nienmd3cn'"""
 
+class User:
+    def __init__(self, uname, fname, email, balance):
+        self.username = uname
+        self.fullname = fname
+        self.email = email
+        self.balance = balance
+
+
 @app.route("/")
 def index():
     return render_template('index.html')
@@ -65,6 +73,14 @@ def login():
         if password == _Password:
             session['online'] = 1
             session['Username'] = _Username
+            statement = """SELECT * FROM person WHERE username = '%s'
+            """ % _Username
+            cursor.execute(statement)
+            row = cursor.fetchone()
+            ActiveUser = User(row[0], row[1], row[2], row[3])
+            session['Fullname'] = row[1]
+            session['Email'] = row[2]
+            session['Balance'] = str(row[4])
             return redirect(url_for('userpage'))
         else:
             return render_template('errorpage.html', message = 'Wrong username/password!')
@@ -78,6 +94,8 @@ def login():
 def register():
     _Username = request.form['username']
     _Password = request.form['password']
+    _Fullname = request.form['name']
+    _Email    = request.form['email']
     connection = dbapi2.connect(dsn)
     cursor = connection.cursor()
     statement = """SELECT * FROM users WHERE username = '%s'
@@ -86,18 +104,26 @@ def register():
     if cursor.rowcount > 0:
         return render_template('errorpage.html', message= 'This username already exists!')
 
-    statement = """INSERT INTO users (username, "password") VALUES (%s, %s)
+    statement = """INSERT INTO users (username, password) VALUES (%s, %s)
     """
     cursor.execute(statement, (_Username, _Password))
+    statement = """INSERT INTO person (username, fullname, emailaddress, userrole) VALUES (%s, %s, %s, %s)
+    """
+    cursor.execute(statement, (_Username, _Fullname, _Email, 'P'))
     connection.commit()
-    session['online'] = 1
+    session['online']   = 1
     session['Username'] = _Username
+    ActiveUser = User(_Username, _Fullname, _Email, 0)
+    session['User'] = ActiveUser
+    session['Fullname'] = _Fullname
+    session['Email'] = _Email
+    session['Balance'] = 0
     return redirect(url_for('userpage'))
 
 @app.route("/userpage/")
 def userpage():
     if 'online' in session:
-        return render_template('userpage.html', username = session['Username'])
+        return render_template('userpage.html')
     else:
         return render_template('errorpage.html', message = 'Not Authorized!')
 
