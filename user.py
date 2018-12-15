fb92784d397722c468d1386ae13fcbbc6ce4f421
saiddikici <43721921+saiddikici@users.noreply.github.com>
2018-12-15 21:25:54 +0300

    qwer

--- user.py
+++
@@ -1,8 +0,0 @@
-import psycopg2 as dbapi2
-from flask import redirect, url_for, request, session, flash
-from general import RenderTemplate, refreshUserData, ifAdmin
-from forms import formForgotPass, formRegister, formEditUser
-import mailsender
-
-dsn = """user='kbktqbcfmdxpbw' password='76006678dc4edef0501db56d75112cacde489dfb1be1648833f8ea853a1e32f4'
-         host='ec2-54-247-101-191.eu-west-1.compute.amazonaws.com' port=5432 dbname='d1lo8nienmd3cn'"""
